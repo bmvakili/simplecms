28f6f866fb671f124f0e8c94a7771a9e288b4c92
rxp8385 <rxp8385@gmail.com>
2015-07-17 10:18:18 -0400

    Simple CMS interface/class and Junit/Mockito Test Cases

---
+++ src/main/java/com/simpleCMS/AddFilesAndFolders.java
@@ -0,0 +1,10 @@
+package com.simpleCMS;
+
+/**
+ * Created by Lando on 7/16/2015.
+ */
+public interface AddFilesAndFolders {
+
+    public boolean addFile();
+    public boolean addFolder();
+}
---
+++ src/test/java/com/simpleCMS/AddInfoTest.java
@@ -0,0 +1,25 @@
+package com.simpleCMS;
+
+import org.junit.Test;
+
+/**
+ * Created by Lando on 7/16/2015.
+ */
+public class AddInfoTest {
+
+    @Test
+
+    public void testAddFiles(){
+        /**
+         * Should pass if...
+         *      a file with an appropriately formatted name
+         *      is added to the corre
+         */
+    }
+
+    @Test
+
+    public void testAddFolders(){
+
+    }
+}
