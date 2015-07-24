bd0663359d65482b104bd3d49b876e4f1682a451
Bijan Vakili <bmvakili@gmail.com>
2015-07-24 13:36:44 -0400

    31196 comment out the javascript that relies on YUI3

--- simplecms-portlet/src/main/webapp/js/main.js
+++ simplecms-portlet/src/main/webapp/js/main.js
@@ -1,3 +1,17 @@
+/*
+* This is an example using YUI framework
+* You can adapt this to other JS frameworks
+* Though key to remember is to prefix the 
+* parameter name with:
+*      simpleCMS.namespace
+* and to call the Resource phase of the
+* SimpleCMS portlet by calling the URL:
+*      simpleCMS.disptacherURL
+* regardless of the JS framework you're using.
+* Which is most likely AngularJS and/or similar
+* JS frameworks.
+*/
+
 YUI().use('node','io','event',
 	function (Y) {
 		var jsonObj = {
